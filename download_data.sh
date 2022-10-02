a671c5734450ff432d5e9dcdafd1d54397c88ab4
Maciej Lewandowski <lewandowski.maciej1702@gmail.com>
2022-10-02 21:09:26 +0100

    latest notebook, still need cleanup

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+mkdir data/CERN/ParticleClassification
+
+wget https://cernbox.cern.ch/index.php/s/AtBT8y4MiQYFcgc/download -O SinglePhotonPt50_IMGCROPS_n249k_RHv1.hdf5
+mv SinglePhotonPt50_IMGCROPS_n249k_RHv1.hdf5 data/CERN/ParticleClassification/SinglePhotonPt50_IMGCROPS_n249k_RHv1.hdf5
+wget https://cernbox.cern.ch/index.php/s/FbXw3V4XNyYB3oA/download -O SingleElectronPt50_IMGCROPS_n249k_RHv1.hdf5
+mv SingleElectronPt50_IMGCROPS_n249k_RHv1.hdf5 data/CERN/ParticleClassification/SingleElectronPt50_IMGCROPS_n249k_RHv1.hdf5
